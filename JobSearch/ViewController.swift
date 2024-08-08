e20307b8c67f50d08a81e3f25e926c9a95143ba0
igorpustylnik <darthveider36@gmail.com>
2024-08-08 13:00:48 +0300

    Initial Commit

---
+++ JobSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobSearch
+//
+//  Created by Игорь Пустыльник on 08.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
