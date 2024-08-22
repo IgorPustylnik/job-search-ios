c17f3bebe125eda7906a2e32f263278fcc20ffba
igorpustylnik <darthveider36@gmail.com>
2024-08-22 13:29:47 +0700

    Add TabBar

--- JobSearch/Components/TabBar/JSTabBarController.swift
+++ JobSearch/Components/TabBar/JSTabBarController.swift
@@ -7,10 +7,26 @@
 
 import UIKit
 
-class JSTabBarController: UITabBarController {
+class JSTabBarController: UITabBarController, UITabBarControllerDelegate {
+    private var buttonContainer: UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        delegate = self
+        let tabBar = JSTabBar()
+        setValue(tabBar, forKey: "tabBar")
+
+        setupTabBar()
+        setupViewControllers()
     }
 
+    private func setupTabBar() {
+        tabBar.shadowImage = UIImage()
+        tabBar.backgroundImage = UIImage()
+    }
+
+    private func setupViewControllers() {
+        viewControllers = JSTabBarModel().controllers
+    }
 }
